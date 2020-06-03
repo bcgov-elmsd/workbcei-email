8a04e3662f8e6c8475c2c65ec54c172561e2fa2a
Solorzano <Rafael.Solorzano@gov.bc.ca>
2020-06-03 16:30:23 -0700

    updated email subject

--- src/routes.js
+++ src/routes.js
@@ -123,7 +123,7 @@ router.post(
       let message = {
         from: 'TRF Referral <donotreply@gov.bc.ca>', // sender address
         to: "Test <ELMSD.Webmaster@gov.bc.ca>", // list of receivers
-        subject: "New TRF Referral - " + data.firstname + " " + data.firstname, // Subject line
+        subject: "New TRF Referral - " + data.firstname + " " + data.lastname, // Subject line
         text: createEmailContactPlainText(data), // plain text body
         html: createEmailContactHtml(data) // html body
       }
